f1c324e0a85ba13dd92bb92404f8c88ce9265b7a
Duda Furtado <furtadoduda@hotmial.com>
2022-04-13 19:27:45 -0300

    Routes were created and and react-router-dom installed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import Login from './pages/LogIn';
-// import SignIn from './pages/SignIn';
-import Home from './pages/Home';
+
+import { BrowserRouter } from 'react-router-dom';
+import MainRoutes from './routes';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Login />  */}
-    {/* <SignIn /> */}
-    <Home />
+    <BrowserRouter>
+      <MainRoutes />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,28 @@
-import { Routes, Route, Outlet, Navigate } from 'react-router-dom'
+import Home from '../src/pages/Home';
+import SignIn from '../src/pages/SignIn';
+import LogIn from '../src/pages/LogIn';
 
-// function ProtectedRoutes ({ redirectTo }) {
-// const isAuthenticated = getItem('token')
-// return isAuthenticated ? <Outlet /> : <Navigate to={redirectTo} />
-//}
+import { getItem } from './utils/storage'
+
+import { Routes, Route, Outlet, Navigate } from 'react-router-dom';
+
+function ProtectedRoutes ({ redirectTo }) {
+    const isAuthenticated = getItem('token');
+    return isAuthenticated ? <Outlet /> : <Navigate to={redirectTo} />
+}
+
+function MainRoutes() {
+    return (
+        <Routes>
+            <Route path={['/', '/login']} element={<LogIn />} />
+            <Route path='/cadastrar' element={<SignIn />} />
+            <Route element={<ProtectedRoutes redirectTo='/cadastrar' />}>
+                <Route 
+                path={['/home', '/usuario', '/categoria', '/transacao/extrato', '/transacao', '/transacao/:id']} 
+                element={<Home />} />
+            </Route>
+        </Routes>
+    );
+}
+
+export default MainRoutes;
